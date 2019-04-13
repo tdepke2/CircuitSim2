c09299c503aad16828fa118e4ee8987ae9e68608
Thomas Depke <tdepke@mymail.mines.edu>
2019-04-12 22:59:00 -0600

    Started working on simulation algorithm v1 and added test case graphic.

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,3 @@ using namespace std;
 int main() {
     return Simulator::start();
 }
-
-/*
-Notes:
-  - Gonna need additional tri-state (dashed color) and conflict (red color) wire states. These will save as unpowered wires however.
-  - Implement the original wire algorithm first for backwards compatibility.
-  - Tile drawing should be changed so that making modifications to a tile does not immediately redraw it, redraw should happen right before actual drawing.
-*/
