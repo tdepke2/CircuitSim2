294494e8c22e5d048690c17f1371347f3f26ddbd
Thomas Depke <tommydepke@gmail.com>
2023-10-03 23:16:59 -0600

    Added more features to FlatMap from std::map, and some tests.

--- src/exp/FlatMap.h
+++ src/exp/FlatMap.h
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <functional>
+#include <initializer_list>
 #include <utility>
 #include <vector>
 
@@ -24,14 +25,103 @@ public:
     using const_pointer = const value_type*;
     using iterator = typename std::vector<value_type>::iterator;
     using const_iterator = typename std::vector<value_type>::const_iterator;
+    using reverse_iterator = typename std::vector<value_type>::reverse_iterator;
+    using const_reverse_iterator = typename std::vector<value_type>::const_reverse_iterator;
 
-    FlatMap() :
+    FlatMap(const Compare& comp = Compare()) :
         vec_(),
-        comp_() {
+        comp_(comp),
+        valueToKeyComp_(comp),
+        valueToValueComp_(comp) {
     }
 
-    T& at(const Key& key) {
+    template<typename InputIt>
+    FlatMap(InputIt first, InputIt last, const Compare& comp = Compare()) :
+        vec_(first, last),
+        comp_(comp),
+        valueToKeyComp_(comp),
+        valueToValueComp_(comp) {
 
+        std::sort(vec_.begin(), vec_.end(), valueToValueComp_);
+        auto newLast = std::unique(vec_.begin(), vec_.end(), [this](const auto& lhs, const auto& rhs) {
+            return !comp_(lhs.first, rhs.first);
+        });
+        vec_.erase(newLast, vec_.end());
+    }
+
+    FlatMap(std::initializer_list<value_type> init, const Compare& comp = Compare()) :
+        FlatMap(init.begin(), init.end(), comp) {
+    }
+
+    T& at(const Key& key) {    // FIXME how to avoid the code duplication with below?
+        auto val = find(key);
+        if (val == vec_.end()) {
+            throw std::out_of_range("FlatMap::at");
+        }
+        return val->second;
+    }
+    const T& at(const Key& key) const {
+        auto val = find(key);
+        if (val == vec_.end()) {
+            throw std::out_of_range("FlatMap::at");
+        }
+        return val->second;
+    }
+    T& operator[](const Key& key) {
+        auto pos = std::lower_bound(vec_.begin(), vec_.end(), key, valueToKeyComp_);
+        if (pos == vec_.end() || comp_(key, pos->first)) {
+            return vec_.emplace(pos, std::piecewise_construct, std::forward_as_tuple(key), std::tuple<>())->second;
+        } else {
+            return pos->second;
+        }
+    }
+    T& operator[](Key&& key) {
+        auto pos = std::lower_bound(vec_.begin(), vec_.end(), key, valueToKeyComp_);
+        if (pos == vec_.end() || comp_(key, pos->first)) {
+            return vec_.emplace(pos, std::piecewise_construct, std::forward_as_tuple(std::move(key)), std::tuple<>())->second;
+        } else {
+            return pos->second;
+        }
+    }
+
+    iterator begin() noexcept {
+        return vec_.begin();
+    }
+    const_iterator begin() const noexcept {
+        return vec_.begin();
+    }
+    const_iterator cbegin() const noexcept {
+        return vec_.cbegin();
+    }
+
+    iterator end() noexcept {
+        return vec_.end();
+    }
+    const_iterator end() const noexcept {
+        return vec_.end();
+    }
+    const_iterator cend() const noexcept {
+        return vec_.cend();
+    }
+
+    reverse_iterator rbegin() noexcept {
+        return vec_.rbegin();
+    }
+    const_reverse_iterator rbegin() const noexcept {
+        return vec_.rbegin();
+    }
+    const_reverse_iterator crbegin() const noexcept {
+        return vec_.crbegin();
+    }
+
+    reverse_iterator rend() noexcept {
+        return vec_.rend();
+    }
+    const_reverse_iterator rend() const noexcept {
+        return vec_.rend();
+    }
+    const_reverse_iterator crend() const noexcept {
+        return vec_.crend();
     }
 
     bool empty() const noexcept {
@@ -40,37 +130,72 @@ public:
     size_type size() const noexcept {
         return vec_.size();
     }
+    size_type max_size() const noexcept {
+        return vec_.max_size();
+    }
 
     void clear() noexcept {
         vec_.clear();
     }
     std::pair<iterator, bool> insert(const value_type& value) {
-        return emplace(value);
+        std::cout << "FlatMap::insert({" << value.first << ", " << value.second << "})\n";
+        auto pos = std::lower_bound(vec_.begin(), vec_.end(), value.first, valueToKeyComp_);
+        std::cout << "lower_bound found at " << pos - vec_.begin() << "\n";
+        if (pos == vec_.end() || comp_(value.first, pos->first)) {
+            std::cout << "new element added\n";
+            return {vec_.insert(pos, value), true};
+        } else {
+            std::cout << "duplicate detected.\n";
+            return {pos, false};
+        }
     }
     std::pair<iterator, bool> insert(value_type&& value) {
-        return emplace(std::move(value));
+        std::cout << "FlatMap::insert(rvalue ref {" << value.first << ", " << value.second << "})\n";
+        auto pos = std::lower_bound(vec_.begin(), vec_.end(), value.first, valueToKeyComp_);
+        std::cout << "lower_bound found at " << pos - vec_.begin() << "\n";
+        if (pos == vec_.end() || comp_(value.first, pos->first)) {
+            std::cout << "new element added\n";
+            return {vec_.insert(pos, std::move(value)), true};
+        } else {
+            std::cout << "duplicate detected.\n";
+            return {pos, false};
+        }
     }
     template<typename... Args>
     std::pair<iterator, bool> emplace(Args&&... args) {
-        value_type&& value(std::forward<Args>(args)...);    // FIXME is this the right idea? #########################
+        // Constructing a temporary to pass to insert() probably isn't the best
+        // way to do this, boost::container::flat_map does something fancy here.
 
-        std::cout << "FlatMap::emplace({" << value.first << ", " << value.second << "})\n";
-        auto pos = std::lower_bound(vec_.begin(), vec_.end(), value, comp_);
-        std::cout << "lower_bound found at " << pos - vec_.begin() << "\n";
-        if (pos == vec_.end() || comp_(value, *pos)) {
-            std::cout << "new element added\n";
-            return std::make_pair(vec_.insert(pos, std::move(value)), true);
-        } else {
-            std::cout << "duplicate detected.\n";
-            return std::make_pair(pos, false);
-        }
+
+        // FIXME idea: use TMP to specialize two emplace functions, one accepts K, V types and other accepts value_type.
+        // now we can get the key without the temporary
+        // hang on, pair can be constructed with piecewise, maybe treat that as special case?
+
+
+        return insert(value_type(std::forward<Args>(args)...));
     }
     iterator erase(iterator pos) {
 
     }
 
+    size_type count(const Key& key) const {
+        return find(key) == vec_.end() ? 0 : 1;
+    }
     iterator find(const Key& key) {
-        
+        auto pos = std::lower_bound(vec_.begin(), vec_.end(), key, valueToKeyComp_);
+        if (pos == vec_.end() || comp_(key, pos->first)) {
+            return vec_.end();
+        } else {
+            return pos;
+        }
+    }
+    const_iterator find(const Key& key) const {
+        auto pos = std::lower_bound(vec_.begin(), vec_.end(), key, valueToKeyComp_);
+        if (pos == vec_.end() || comp_(key, pos->first)) {
+            return vec_.end();
+        } else {
+            return pos;
+        }
     }
 
     std::vector<value_type>& debugGetVec() {
@@ -78,13 +203,27 @@ public:
     }
 
 private:
-    struct ValueComparator {
+    struct ValueToKeyComparator {
+        ValueToKeyComparator(const Compare& c) :
+            c(c) {
+        }
+        inline bool operator()(const value_type& lhs, const key_type& rhs) {
+            return c(lhs.first, rhs);
+        }
+        Compare c;
+    };
+    struct ValueToValueComparator {
+        ValueToValueComparator(const Compare& c) :
+            c(c) {
+        }
         inline bool operator()(const value_type& lhs, const value_type& rhs) {
-            Compare c;    // FIXME should this be instantiated as a member instead? ########################
             return c(lhs.first, rhs.first);
         }
+        Compare c;
     };
 
     std::vector<value_type> vec_;
-    ValueComparator comp_;
+    Compare comp_;
+    ValueToKeyComparator valueToKeyComp_;
+    ValueToValueComparator valueToValueComp_;
 };
--- src/exp/FlatMap.test.cpp
+++ src/exp/FlatMap.test.cpp
@@ -5,36 +5,205 @@
 #include <spdlog/spdlog.h>
 #include <string>
 
+template<typename T>
+struct CustomComp {
+    CustomComp() {
+        std::cout << "CustomComp::CustomComp()\n";
+    }
+    inline bool operator()(const T& lhs, const T& rhs) {
+        return lhs > rhs;
+    }
+};
+
+template<typename Key, typename Value, typename Compare>
+std::ostream& operator<<(std::ostream& out, const std::map<Key, Value, Compare>& map) {
+    out << "{ ";
+    for (const auto& v : map) {
+        out << "[" << v.first << "] -> " << v.second << ", ";
+    }
+    return out << "}\n";
+}
+
+template<typename Key, typename Value, typename Compare>
+std::ostream& operator<<(std::ostream& out, const FlatMap<Key, Value, Compare>& map) {
+    out << "{ ";
+    for (const auto& v : map) {
+        out << "[" << v.first << "] -> " << v.second << ", ";
+    }
+    return out << "}\n";
+}
+
+struct Point {
+    double x, y;
+    friend std::ostream& operator<<(std::ostream& out, Point pt) {
+        return out << "(" << pt.x << ", " << pt.y << ")";
+    }
+};
+
+struct PointCmp {
+    bool operator()(const Point& lhs, const Point& rhs) const {
+        return lhs.x < rhs.x;
+    }
+};
+
+template<template<typename...> class Map>
+void testCtor() {
+    // Modified example code from en.cppreference.com
+
+    // Default constructor.
+    Map<std::string, int> map1;
+    map1["something"] = 69;
+    map1["anything"] = 199;
+    map1["that thing"] = 50;
+    map1["another thing"] = 123;
+    std::cout << "default ctor = " << map1 << "\n";
+
+    // Range constructor.
+    Map<std::string, int> range1(map1.find("anything"), map1.end());
+    std::cout << "range ctor = " << range1 << "\n";
+
+    Map<std::string, int> range2(map1.rbegin(), map1.rend());
+    std::cout << "range ctor (reversed) = " << range2 << "\n";
+
+    // Copy constructor.
+    Map<std::string, int> copied1(map1);
+    std::cout << "copy ctor = " << copied1 << "\n";
+
+    // Move constructor.
+    Map<std::string, int> moved1(std::move(map1));
+    std::cout << "move ctor = " << moved1 << "\n";
+
+    // Initializer list constructor.
+    const Map<std::string, int> init1 {
+        {"this", 100},
+        {"can", 100},
+        {"be", 100},
+        {"const", 100}
+    };
+    std::cout << "init list ctor = " << init1 << "\n";
+
+    const Map<std::string, int> init2 {
+        {"hello", 1},
+        {"this", 2},
+        {"is", 3},
+        {"a", 4},
+        {"this", 6},
+        {"test", 5}
+    };
+    std::cout << "init list ctor (dupes) = " << init2 << "\n";
+
+    const Map<std::string, int> init3 {
+        {"hello", 5},
+        {"hello", 4},
+        {"hello", 3},
+        {"hello", 2},
+        {"hello", 1}
+    };
+    std::cout << "init list ctor (lot of dupes) = " << init3 << "\n";
+
+    // Custom comparator.
+    Map<Point, double, PointCmp> magnitudes1 = {
+        {{4, 3}, 5},
+        {{5, -12}, 13},
+        {{3, 4}, 5},
+        {{-8, -15}, 17}
+    };
+    std::cout << "custom comp = " << magnitudes1 << "\n";
+
+    // This lambda sorts points according to their magnitudes.
+    auto cmpLambda = [&magnitudes1](const Point& lhs, const Point& rhs) {
+        return magnitudes1[lhs] < magnitudes1[rhs];
+    };
+    Map<Point, double, decltype(cmpLambda)> magnitudes2({
+        {{4, 3}, 5}
+    }, cmpLambda);
+
+    magnitudes2.insert(std::pair<Point, double>({5, -12}, 13));
+    magnitudes2.insert({{3, 4}, 5});
+    magnitudes2.insert({Point{-8.0, -15.0}, 17});
+    std::cout << "custom lambda comp = " << magnitudes2 << "\n";
+}
+
+template<template<typename...> class Map>
+void testAccess() {
+    Map<std::string, std::string> myMap;
+    std::cout << "empty myMap = " << myMap << "\n";
+
+    try {
+        std::cout << "  item [test_at] = " << myMap.at("test_at") << "\n";
+    } catch (std::out_of_range& ex) {
+        std::cout << "  item [test_at] exception: " << ex.what() << "\n";
+    }
+    std::cout << "  item [test_op_brackets] = " << myMap["test_op_brackets"] << "\n";
+    myMap["new"] = "element";
+    myMap["another"];
+    myMap.at("another") = "element!";
+    std::cout << "modified myMap = " << myMap << "\n";
+
+    const Map<std::string, std::string> myMap2 {
+        {"hello", "test"},
+        {"numbers", "123"}
+    };
+    std::cout << "  item [hello] = " << myMap2.at("hello") << "\n";
+    // Below should not compile (brackets operator isn't const).
+    //std::cout << "  item [numbers] = " << myMap2["numbers"] << "\n";
+    std::cout << "myMap2 = " << myMap2 << "\n";
+
+    Map<std::string, int> wordMap;
+    auto words = {
+        "this", "sentence", "is", "not", "a", "sentence", "this", "sentence", "is", "a", "hoax"
+    };
+    for (const auto& w : words) {
+        ++wordMap[w];
+    }
+    wordMap["none"];
+    std::cout << "wordMap = " << wordMap << "\n";
+}
+
 int main() {
     spdlog::info("Started FlatMap test.");
 
     std::map<int, std::string> stuff;
     stuff.key_comp();
 
-    std::less<int> comp;
+    /*std::less<int> comp;
     spdlog::info("1<2 is {}, 4<3 is {}", comp(1, 2), comp(4, 3));
 
     std::map<int, std::string> m;
-    m.insert(std::make_pair(1, "hello"));
-    m.insert({std::make_pair(2, "world"), std::make_pair(3, "test")});
+    m.insert(decltype(m)::value_type(1, "hello"));
+    m.insert({decltype(m)::value_type(2, "world"), decltype(m)::value_type(3, "test")});
     m.emplace(4, "...");
 
-    FlatMap<int, std::string> flat;
-    flat.insert(std::make_pair(1, "hello"));
+    FlatMap<int, std::string, CustomComp<int>> flat;
+    auto val = decltype(flat)::value_type(1, "hello");
+    flat.insert(val);
     flat.insert({5, "test"});
-    flat.insert(std::make_pair(3, "is"));
-    flat.insert(std::make_pair(2, "this"));
-    flat.insert(std::make_pair(4, "a"));
+    flat.insert(decltype(flat)::value_type(3, "is"));
+    flat.insert(decltype(flat)::value_type(2, "this"));
+    flat.insert(decltype(flat)::value_type(4, "a"));
 
-    flat.insert(std::make_pair(3, "isn\'t"));
-    flat.insert(std::make_pair(-3, "start"));
-    flat.insert(std::make_pair(10, "end"));
+    flat.emplace(3, "isn\'t");
+    flat.emplace(-3, "start");
+    flat.emplace(10, "end");
+
+    m.at(1) = "cool";
+    flat.at(1) = "cool";
 
     size_t i = 0;
     for (const auto& x : flat.debugGetVec()) {
         std::cout << "[" << i << "] " << x.first << " -> [" << x.second << "]\n";
         ++i;
-    }
+    }*/
+
+    std::cout << "\n\ntestCtor<std::map>()\n";
+    testCtor<std::map>();
+    std::cout << "\ntestCtor<FlatMap>()\n";
+    testCtor<FlatMap>();
+
+    std::cout << "\n\ntestAccess<std::map>()\n";
+    testAccess<std::map>();
+    std::cout << "\ntestAccess<FlatMap>()\n";
+    testAccess<FlatMap>();
 
     return 0;
 }
