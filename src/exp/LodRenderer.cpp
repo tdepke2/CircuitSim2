d701159df4f183f72114e231a334a34803d26247
Thomas Depke <tommydepke@gmail.com>
2023-12-19 23:18:32 -0700

    Added stub class LodRenderer to abstract functionality with chunk-based rendering.

---
+++ src/exp/LodRenderer.cpp
@@ -0,0 +1,5 @@
+#include <LodRenderer.h>
+
+int LodRenderer::getCurrentLod() const {
+    return currentLod_;
+}
---
+++ src/exp/LodRenderer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class OffsetView;
+
+class LodRenderer {
+public:
+    virtual ~LodRenderer() noexcept = default;
+    // FIXME should be private nonvirtual? probably should add virtual to other derived classes that missed it.
+    LodRenderer(const LodRenderer& rhs) = default;
+    LodRenderer& operator=(const LodRenderer& rhs) = default;
+
+    // FIXME include some helper functions to abstract functionality from Board and ChunkGroup, maybe skip the virtual methods.
+    virtual void setRenderArea(const OffsetView& offsetView, float zoom) = 0;
+    int getCurrentLod() const;
+
+private:
+    int currentLod_;
+};
